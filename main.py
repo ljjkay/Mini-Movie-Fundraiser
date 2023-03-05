324a2dadb2eb90ff6867c8f272d26ed01eaf1ac2
Louis Kay <104738315+ljjkay@users.noreply.github.com>
2023-03-05 22:13:16 +0000

    Tickets are now avaible time to do age and names

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-MAX_TICKETS  = 3
+def yes_no(question):
+  while True:
+    response = input(question).lower()
 
+    if response == "yes" or response == "y":
+      return "yes"
+    elif response == "no" or response == "n":
+      return "no"
+    else:
+      print("Please enter yes or no")
+
+
+MAX_TICKETS  = 3
 tickets_sold = 0
+
+want_instructions = yes_no("Do you want the instructions? ")
+if want_instructions == "yes":
+  print("Instruction go here")
+
+print()
+
 while tickets_sold < MAX_TICKETS:
   name = input("Please enter your name or 'xxx' to quit ")
   if name == 'xxx':
@@ -8,7 +26,7 @@ while tickets_sold < MAX_TICKETS:
 
   tickets_sold += 1
 
-  if tickets_sold == MAX_TICKETS:
-    print("Congragulation you have sold all the tickets")
+  if tickets_sold ==MAX_TICKETS:
+    print("Congraulation you have sold all the tickets")
   else:
-    print("you have sold {} ticket/s. There is {} ticket/s left")
+    print("you have sold {} ticket/s. There is {} ticket/s left".format(tickets_sold, MAX_TICKETS - tickets_sold))
